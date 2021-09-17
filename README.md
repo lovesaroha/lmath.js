fec53452c784a104e45f82ee2affa7a725b956a2
LoveSaroha <lovesaroha1994@gmail.com>
2021-09-17 18:42:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,198 @@
+## lmath.js
+This is a generalized math package with clean and transparent API for the javascript.
+Also available for golang [github/lovesaroha/lmath](https://github.com/lovesaroha/lmath) 
+
+## Installation
+
+```html
+    <script type="text/javascript" src="lmath.js"></script>
+```
+
+## Usage
+
+### Random
+
+```js
+  // Random value between given range.
+  let random = lmath.Random(-1, 1)
+```
+
+### Map Given Value Between Given Range
+```js
+  // Return value between given range of (200, 1000).
+  let value = lmath.Map(20 , 0, 100, 200, 1000)
+```
+
+## Apply Math Functions
+```js
+  // Sigmoid.
+  let result = lmath.Sigmoid(1.5)
+  // Diff of sigmoid.
+  let dresult = lmath.Dsigmoid(result)
+  // Relu.
+  let result = lmath.Relu(2)
+  // Diff of relu.
+  let dresult = lmath.Drelu(result)
+```
+### Create Matrix 
+
+```js
+  // Create a matrix (rows, cols).
+  let matrix = lmath.Matrix(4, 3)
+
+  // Print matrix shape.
+  matrix.Shape()
+  // Print matrix values.
+  matrix.Print()
+```
+
+### Create Random Matrix 
+
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, -1 , 1)
+
+  // Print matrix shape.
+  matrix.Shape()
+  // Print matrix values.
+  matrix.Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/39.png)
+
+### Convert Array Into Matrix 
+
+```js
+    // Array to matrix and print values.
+    lmath.ToMatrix([1, 2, 3]).Print()
+    lmath.ToMatrix([[1, 2], [3, 4]]).Print()
+    lmath.ToMatrix([4, 5, 6]).Print()
+        lmath.ToMatrix([[7, 8], [9, 0]]).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/45.png)
+
+### Matrix Element Wise Operations (Add, Subtract, Multiply, Divide) 
+
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+  let matrixB = lmath.Matrix(3, 4, 0, 10)
+
+  // Add and print values.
+  matrix.Add(matrixB).Print()
+  // Subtract and print values.
+  matrix.Sub(matrixB).Print()
+  // Multiply and print values.
+  matrix.Mul(matrixB).Print()
+  // Divide and print values.
+  matrix.Sub(matrixB).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/40.png)
+
+### Matrix Element Wise Operations With Scalar Value (Add, Subtract, Multiply, Divide) 
+
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+
+  // Add and print values.
+  matrix.Add(2).Print()
+  // Subtract and print values.
+  matrix.Sub(2).Print()
+  // Multiply and print values.
+  matrix.Mul(2).Print()
+  // Divide and print values.
+  matrix.Sub(2).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/47.png)
+
+### Matrix Dot Product 
+
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+  let matrixB = lmath.Matrix(4, 3, 0, 10)
+
+  // Dot product and print values.
+  matrix.Dot(matrixB).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/41.png)
+
+### Matrix Transpose
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+
+  // Print values.
+  matrix.Print()
+  // Transpose and print values.
+  matrix.Transpose().Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/42.png)
+
+### Add All Column Values
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+
+  // Print values.
+  matrix.Print()
+  // Add columns and print values.
+  matrix.AddCols().Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/46.png)
+
+### Change Matrix Values (Map)
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, 10, 20)
+
+  // Print values.
+  matrix.Print()
+  // Square and print values.
+  matrix.Map(function(x) {
+      return x * x;
+  }).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/43.png)
+
+### Apply Function In Matrix
+```js
+  // Create a matrix (rows, cols, minimum , maximum).
+  let matrix = lmath.Matrix(3, 4, -1, 1)
+
+  // Print values.
+  matrix.Print()
+  // Apply sigmoid and print values.
+  matrix.Map(lmath.Sigmoid).Print()
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/44.png)
+
+## Examples
+
+### Logistic Regression (OR Gate)
+```js 
+    // Learning rate.
+    let learningRate = 0.2
+
+    // Inputs and outputs.
+    let inputs = lmath.ToMatrix([[1, 1, 0, 0] , [0, 1, 0, 1]])
+    let outputs = lmath.ToMatrix([[1, 1, 0, 1]])
+
+    // Weights and bias.
+    let weights = lmath.Matrix(2, 1, -1, 1)
+    let bias = lmath.Random(-1, 1)
+
+    // Train weights and bias (epochs 1000).
+    for(let i = 0; i < 1000; i++) {
+      // Sigmoid(wx + b).
+      let prediction = weights.Transpose().Dot(inputs).Add(bias).Map(lmath.Sigmoid)
+      let dZ = prediction.Sub(outputs)
+      weights = weights.Sub(inputs.Dot(dZ.Transpose()).Divide(4).Mul(learningRate))
+      bias -= dZ.Sum() / 4
+    }
+
+    // Show prediction.
+    weights.Transpose().Dot(inputs).Add(bias).Map(lmath.Sigmoid).Print()
+
+```
+![image](https://raw.githubusercontent.com/lovesaroha/gimages/main/48.png)
---
+++ lmath.js
@@ -0,0 +1,317 @@
+/*  Love Saroha
+    lovesaroha1994@gmail.com (email address)
+    https://www.lovesaroha.com (website)
+    https://github.com/lovesaroha  (github)
+*/
+
+"use-strict";
+
+(function ($) {
+    // Random number between range.
+    function Random(min, max) {
+        // Checks if min or max are given.
+        if (!min || typeof min != "number") {
+            min = -1;
+        }
+        if (!max || typeof min != "number") {
+            max = 1;
+        }
+        let randNumb = Math.random();
+        if (min < 0) {
+            randNumb *= (max + Math.abs(min));
+        } else {
+            randNumb *= (max - min);
+        }
+        randNumb += min;
+        return randNumb;
+    }
+
+    // Sigmoid function.
+    function Sigmoid(x) {
+        // If x is not a number.
+        if (typeof x != "number") {
+            console.error("lmath.js: Input to sigmoid() is not a valid number!");
+            return 0;
+        }
+        return 1 / (1 + Math.exp(-x));
+    }
+    // Differential of sigmoid.
+    function Dsigmoid(y) {
+        // If y is not a number.
+        if (typeof y != "number") {
+            console.error("lmath.js: Input to dsigmoid() is not a valid number!");
+            return 0;
+        }
+        return y * (1 - y);
+    }
+
+    // Relu function.
+    function Relu(x) {
+        // If x is not a number.
+        if (typeof x != "number") {
+            console.error("lmath.js: Input to relu() is not a valid number!");
+            return 0;
+        }
+        return Math.max(0, x);
+    }
+
+    // Differentiation of relu.
+    function Drelu(y) {
+        // If y is not a number.
+        if (typeof y != "number") {
+            console.error("lmath.js: Input to drelu() is not a valid number!");
+            return 0;
+        }
+        if (y <= 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Map.
+    function MapValue(n, start1, stop1, start2, stop2, withinBounds) {
+        if (typeof n != "number" || typeof start1 != "number" || typeof stop1 != "number" || typeof start2 != "number" || typeof stop2 != "number") {
+            // Checks if any argument is not a number.
+            console.error("lmath.js: Arguments to map() must be a valid number!");
+            return 1;
+        }
+        var newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+        if (!withinBounds) {
+            return newval;
+        }
+        if (start2 < stop2) {
+            return constrain(newval, start2, stop2);
+        } else {
+            return constrain(newval, stop2, start2);
+        }
+    }
+
+    // Constrain.
+    function constrain(n, low, high) {
+        return Math.max(Math.min(n, high), low);
+    }
+
+    // Matrix class.
+    class MatrixObject {
+        // Constructor function for new Matrix Class().
+        constructor(rows, cols) {
+            // Create new matrix.
+            this.rows = rows;
+            this.cols = cols;
+            this.values = [];
+        }
+        // Print matrix shape.
+        Shape() {
+            console.log(`(${this.rows} x ${this.cols})`);
+        }
+        // Print matrix values.
+        Print() {
+            let printLog = ``;
+            for (let i = 0; i < this.rows; i++) {
+                printLog = printLog + ` [ `;
+                for (let j = 0; j < this.cols; j++) {
+                    printLog = printLog + ` ${this.values[i][j]} `;
+                }
+                printLog = printLog + ` ] \n`;
+            }
+            console.log(printLog);
+        }
+        // Add matrix or scaler number to a matrix.
+        Add(value) {
+            return elementWise(this, value, 1);
+        }
+        // Subtract matrix or scaler number to a matrix.
+        Sub(value) {
+            return elementWise(this, value, 2);
+        }
+        // Multiply matrix element wise.
+        Mul(value) {
+            return elementWise(this, value, 3);
+        }
+        // Multiply matrix element wise.
+        Divide(value) {
+            return elementWise(this, value, 4);
+        }
+        // Copy of a matrix.
+        Copy() {
+            let newMatrix = new MatrixObject(this.rows, this.cols);
+            for (let i = 0; i < this.rows; i++) {
+                newMatrix.values[i] = [];
+                for (let j = 0; j < this.cols; j++) {
+                    newMatrix.values[i][j] = this.values[i][j];
+                }
+            }
+            return newMatrix;
+        }
+        // Transpose of a matrix.
+        Transpose() {
+            let newMatrix = new MatrixObject(this.cols, this.rows);
+            for (let i = 0; i < this.cols; i++) {
+                newMatrix.values[i] = [];
+                for (let j = 0; j < this.rows; j++) {
+                    newMatrix.values[i][j] = this.values[j][i];
+                }
+            }
+            return newMatrix;
+        }
+        // Dot product scalar or matrix.
+        Dot(value) {
+            if (value instanceof MatrixObject == false) {
+                console.error("lmath.js: Must be a matrix object!");
+                return this;
+            }
+            // Argument is a matrix object.
+            if (this.cols != value.rows) {
+                console.error("lmath.js: Number of columns of first matrix must be equal to number of rows in second!");
+                return;
+            }
+            let newMatrix = new MatrixObject(this.rows, value.cols);
+            for (let i = 0; i < newMatrix.rows; i++) {
+                newMatrix.values[i] = [];
+                for (let j = 0; j < newMatrix.cols; j++) {
+                    let sum = 0;
+                    for (let r = 0; r < this.cols; r++) {
+                        sum += this.values[i][r] * value.values[r][j];
+                    }
+                    newMatrix.values[i][j] = sum;
+                }
+            }
+            return newMatrix;
+        }
+        // Sum of all values.
+        Sum() {
+            let sum = 0;
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    sum += this.values[i][j];
+                }
+            }
+            return sum;
+        }
+        // Map function.
+        Map(callback) {
+            // Checks for valid callback function.
+            if (callback && typeof callback != "function") {
+                console.error("lmath.js: Argument to map() function must be a function!");
+                return;
+            }
+            let newMatrix = this.Copy();
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    newMatrix.values[i][j] = callback(this.values[i][j]);
+                }
+            }
+            return newMatrix;
+        }
+
+        // Add all column values.
+        AddCols() {
+            let newMatrix = new MatrixObject(this.rows, 1);
+            for (let i = 0; i < this.rows; i++) {
+                let sum = 0;
+                for (let j = 0; j < this.cols; j++) {
+                    sum += this.values[i][j];
+                }
+                newMatrix.values[i] = [sum];
+            }
+        }
+    }
+
+    // Perform addition or subtraction or multiplication element wise.
+    function elementWise(matrix, value, option) {
+        let newMatrix = matrix.Copy();
+        if (value.constructor == Array) {
+            // Convert array to matrix.
+            value = ToMatrix(value);
+        }
+        if (value instanceof MatrixObject) {
+            // Argument is a matrix.
+            if (newMatrix.rows != value.rows || newMatrix.cols != value.cols) {
+                console.error("lmath.js: Number of rows and columns must be same!");
+                return newMatrix;
+            }
+            for (let i = 0; i < newMatrix.rows; i++) {
+                for (let j = 0; j < newMatrix.cols; j++) {
+                    if (option == 1) {
+                        newMatrix.values[i][j] += value.values[i][j];
+                    } else if (option == 2) {
+                        newMatrix.values[i][j] -= value.values[i][j];
+                    } else if (option == 3) {
+                        newMatrix.values[i][j] *= value.values[i][j];
+                    } else {
+                        newMatrix.values[i][j] /= value.values[i][j];
+                    }
+                }
+            }
+        } else if (typeof value == "number") {
+            // Argument is a number.
+            for (let i = 0; i < newMatrix.rows; i++) {
+                for (let j = 0; j < newMatrix.cols; j++) {
+                    if (option == 1) {
+                        newMatrix.values[i][j] += value;
+                    } else if (option == 2) {
+                        newMatrix.values[i][j] -= value;
+                    } else if (option == 3) {
+                        newMatrix.values[i][j] *= value;
+                    } else {
+                        newMatrix.values[i][j] /= value;
+                    }
+                }
+            }
+        }
+        return newMatrix;
+    }
+
+    // Exported function matrix.
+    function Matrix(rows, cols, minimum, maximum) {
+        if (typeof rows != "number" || typeof cols != "number") {
+            console.error("lmath.js: Rows and Columns to Matrix() must be a whole number!");
+            return;
+        }
+        // Create a new matrix.
+        let matrix = new MatrixObject(parseInt(rows), parseInt(cols));
+        minimum = minimum || 0;
+        maximum = maximum || 0;
+        for (let i = 0; i < matrix.rows; i++) {
+            matrix.values[i] = [];
+            for (let j = 0; j < matrix.cols; j++) {
+                matrix.values[i][j] = Random(minimum, maximum);
+            }
+        }
+        return matrix;
+    }
+
+    // This function convert given value to matrix.
+    function ToMatrix(value) {
+        if (value.constructor == Array) {
+            // Convert to matrix.
+            let newMatrix = new MatrixObject(value.length, value[0].length);
+            for (let i = 0; i < newMatrix.rows; i++) {
+                newMatrix.values[i] = [];
+                for (let j = 0; j < newMatrix.cols; j++) {
+                    if (typeof value[i][j] != "number") {
+                        console.error(`lmath.js: Array contains invalid number at ${i}${j}!`);
+                        return newMatrix;
+                    }
+                    newMatrix.values[i][j] = value[i][j];
+                }
+            }
+            return newMatrix;
+        }
+        return new MatrixObject(0, 0);
+    }
+
+
+    // Exported function.
+    $.lmath = {
+        Random: Random,
+        Sigmoid: Sigmoid,
+        Dsigmoid: Dsigmoid,
+        Relu: Relu,
+        Drelu: Drelu,
+        Map: MapValue,
+        Matrix: Matrix,
+        ToMatrix: ToMatrix
+    };
+}(window));
